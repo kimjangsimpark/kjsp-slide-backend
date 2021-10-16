764d72e6c8072c77c414ed899c44f1d8484deda4
김현수 <hyunsuu.kim@gmail.com>
2021-10-16 23:08:19 +0900

    chore: UserInfoController > UserInfoRepository Bean 객체 주입

--- src/main/java/com/kjsp/kjspslide/controller/UserInfoController.java
+++ src/main/java/com/kjsp/kjspslide/controller/UserInfoController.java
@@ -1,6 +1,8 @@
 package com.kjsp.kjspslide.controller;
 
 import com.kjsp.kjspslide.constant.MediaTypeConstant;
+import com.kjsp.kjspslide.repository.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/user", produces = MediaTypeConstant.APPLICATION_REST_JSON_VALUE)
 public class UserInfoController {
+  private final UserInfoRepository userInfoRepository;
+
+  @Autowired
+  public UserInfoController(
+    UserInfoRepository userInfoRepository
+  ) {
+    this.userInfoRepository = userInfoRepository;
+  }
 
   // TODO
   @PostMapping(value = "/signin")
