b1d3417c573b66db1c03f5a114bed7f27635ed7b
duchangkim <duchang.dev@gmail.com>
2021-10-19 23:28:34 +0900

    feat: Add CORS mappings in configuration

--- src/main/java/com/kjsp/kjspslide/configuration/WebMvcConfig.java
+++ src/main/java/com/kjsp/kjspslide/configuration/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebMvc
@@ -18,5 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(bearerAuthInterceptor)
         .addPathPatterns("/api/user/auth/check");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+      .allowedHeaders("*");
   }
 }
